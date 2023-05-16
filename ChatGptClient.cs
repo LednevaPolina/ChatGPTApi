5c8a9c8bff728360c3eab5fbf65cd8db425f5d05
PolinaLedneva <no4nojsnajpera@mail.ru>
2023-05-16 22:50:51 +0300

    Добавьте файлы проекта.

---
+++ ChatGptClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ChatGPTApi
+{
+    class ChatGptClient :IDisposable
+    {
+        private readonly HttpClient _httpClient;
+        public ChatGptClient(string openAiApiKey)
+        {           
+            _httpClient = new HttpClient()
+            {
+                DefaultRequestHeaders =
+                {
+                    Authorization = AuthenticationHeaderValue.Parse($"Bearer {openAiApiKey}")
+                }
+            };
+        }
+        public async Task<string> GetChatGptResponse(string messageUser)
+        {
+           
+            var chatGptRequest = new ChatGptRequest();
+            chatGptRequest.Model = "gpt-3.5-turbo";
+            chatGptRequest.Messages = new MessageRequest[] { new MessageRequest()
+            { RoleRequest = "user", ContentRequest = messageUser } };
+            
+            var chatCompletionUri = "https://api.pawan.krd/v1/chat/completions";
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync(chatCompletionUri, chatGptRequest);
+            ChatGptResponse? chatGptResponse = await response.Content.ReadFromJsonAsync<ChatGptResponse>();
+            return chatGptResponse.Choices[0].Message.Content.ToString();
+        }
+        public void Dispose()
+        {
+            _httpClient.Dispose();
+        }        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using ChatGPTApi;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+string messageUser = "";
+Console.WriteLine("Спросите о чем-нибудь или нажмите 0 для выхода из программы: ");
+var openAiApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+while (true)
+{
+    messageUser = Convert.ToString(Console.ReadLine())!;
+    if (messageUser != "0")
+    {
+        ChatGptClient chatGptClient = new ChatGptClient(openAiApiKey) { };
+        Console.WriteLine(await chatGptClient.GetChatGptResponse(messageUser));
+        messageUser = "";
+    }
+    else
+    {
+        break;
+    }
+}
+Environment.Exit(0);
